61ffd99aa6f553c080a800c68b0adb2789b21228
umer-qayyum <mohammedumer3353@gmail.com>
2024-12-31 23:16:56 +0500

    user updated

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc,deleteDoc } from 'firebase/firestore';
 import { db } from '@/lib/firebase';
 
 interface Params {
@@ -38,3 +38,34 @@ export async function GET(req: Request, { params }: Params) {
     );
   }
 }
+
+// Delete a user by ID (DELETE)
+export async function DELETE(req: Request, { params }: Params) {
+    const { id } = params; // Extracting the user ID from the request params
+  
+    try {
+      const userDoc = doc(db, 'users', id); // Reference to the specific user document
+      const userSnapshot = await getDoc(userDoc);
+  
+      if (!userSnapshot.exists()) {
+        return NextResponse.json(
+          { error: 'User not found' },
+          { status: 404 }
+        );
+      }
+  
+      await deleteDoc(userDoc); // Delete the user document
+      console.log(`User with ID ${id} deleted successfully.`);
+  
+      return NextResponse.json(
+        { message: 'User deleted successfully' },
+        { status: 200 }
+      );
+    } catch (error) {
+      console.error('Error deleting user:', error);
+      return NextResponse.json(
+        { error: 'Failed to delete user' },
+        { status: 500 }
+      );
+    }
+  }
